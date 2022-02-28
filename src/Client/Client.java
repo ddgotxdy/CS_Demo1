ba12042bf8806c086d90433dfe75ea3f6db04857
ddgo <1812805089@qq.com>
2022-02-28 22:06:29 +0800

    客户端能多次上传图片到服务器上

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -9,36 +9,64 @@ public class Client extends Thread{
     private static int port = 6060;
     private static Scanner scanner = new Scanner(System.in);
     // 访问的ip地址
-    private static String Servername = "182.92.216.59";
+//    private static String Servername = "182.92.216.59";
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
+        InetAddress inetAddress = null;
+        Socket client = null;
+        FileInputStream fileInputStream = null;
         try {
 //            本地测试
-//            InetAddress inetAddress = InetAddress.getLocalHost();
-//            Socket client = new Socket(inetAddress.getHostName(),port);
+            inetAddress = InetAddress.getLocalHost();
+            client = new Socket(inetAddress.getHostName(),port);
 //            远程服务器测试，需要配置内网穿透
-            Socket client = new Socket(Servername,port);
+//            Socket client = new Socket(Servername,port);
             DataInputStream in = new DataInputStream(client.getInputStream());
             DataOutputStream out = new DataOutputStream(client.getOutputStream());
             File file = null;
+            String path = null;
             do {
-                String path = scanner.nextLine();
+                System.out.print("输入正确的图片路径（exit退出）：");
+                // 输入文件路径
+                path = scanner.nextLine();
                 file = new File(path);
-                if(file.isFile()) break;
-                else System.out.println("Error File name or is directory");
-            }while(true);
-            FileInputStream fileInputStream = new FileInputStream(file);
-
-            out.writeUTF("submit");
-            out.writeUTF(file.getName());
-            int b;
-            do {
-                b = fileInputStream.read();
-                out.write(b);
-            }while(b != -1);
-            client.close();
+                // 是文件才处理，是目录等不处理
+                if(file.isFile()) {
+                    fileInputStream = new FileInputStream(file);
+                    out.writeUTF("submit");
+                    out.writeUTF(file.getName());
+                    // 创建文件IO流进行字节传输
+                    int b;
+                    do {
+                        b = fileInputStream.read();
+                        // 为了处理到文件末尾，直接传int
+                        out.writeInt(b);
+                    }while(b != -1);
+                    System.out.println("传输成功");
+                } else if(!path.equals("exit")) {
+                    System.out.println("Error File name or is directory");
+                }
+            }while(!path.equals("exit"));
+            out.writeUTF("exit");
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if(client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
     }
 
--- src/Server/Response.java
+++ src/Server/Response.java
@@ -1,9 +1,6 @@
 package Server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class Response {
@@ -16,36 +13,81 @@ public class Response {
         return ResponseSingle.response;
     }
 
-    public void slove(Socket socket) throws Exception{
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        String query = in.readUTF();
-        if(query.equals("submit")) submit(socket);
-        else {
-            out.writeUTF("Error Comand");
+
+    /**
+     * 传入Socket，与客户端进行交互，处理客户端发来的信号
+     * @param socket
+     */
+    public void slove(Socket socket) {
+        DataInputStream in = null;
+        DataOutputStream out = null;
+
+        try {
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            String query = null;
+            do {
+                query = in.readUTF();
+                if(query.equals("submit")) submit(socket);
+            } while(!query.equals("exit"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        socket.close();
     }
 
-    private void submit(Socket socket) throws Exception {
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        String path = File.separator + "root" + File.separator + "java" + File.separator
-                + "CS_demo1_tmp" + socket.getInetAddress().toString() + "_" + in.readUTF();
-//        String path = "G:" + File.separator + socket.getInetAddress().toString() + "_" + in.readUTF();
-        File file = new File(path);
-        File fileParent = file.getParentFile();
-        if(!fileParent.exists()) {
-            fileParent.mkdirs();
+    /**
+     * 传输图片
+     * @param socket
+     */
+    private void submit(Socket socket) {
+        DataInputStream in = null;
+        DataOutputStream out = null;
+        FileOutputStream fileOutputStream = null;
+        try {
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            //        String path = File.separator + "root" + File.separator + "java" + File.separator
+//                + "CS_demo1_tmp" + socket.getInetAddress().toString() + "_" + in.readUTF();
+            // 存放的位置
+            String path = "G:" + File.separator + socket.getInetAddress().toString() + "_" + in.readUTF();
+            File file = new File(path);
+            File fileParent = file.getParentFile();
+            // 判断父目录是否存在，不存在则创建
+            if(!fileParent.exists()) {
+                fileParent.mkdirs();
+            }
+            // 判断文件是否存在，不存在则创建
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            // 向文件里面输入内容
+            fileOutputStream = new FileOutputStream(file);
+            int b;
+            do {
+                b = in.readInt();
+                // 只要低8位
+                if(b < 256) fileOutputStream.write(b & 0xff);
+            } while(b != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭IO流
+            if(fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        if(!file.exists()) {
-            file.createNewFile();
-        }
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-        do {
-            int b = in.read();
-            if (b == -1) break;
-            fileOutputStream.write(b);
-        } while (true);
+
     }
 }
